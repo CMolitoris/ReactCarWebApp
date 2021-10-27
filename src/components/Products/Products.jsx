b3e04591a653733b73d8aa4fa34633b07f64b420
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-27 14:26:32 -0400

    Updated products page.
    - Added add to cart button.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Button} from 'react-bootstrap';
 
 
 const Products = (props) => {
@@ -9,38 +9,49 @@ const Products = (props) => {
 
 
     return ( 
-        <Row xs={1} md={3} className="g-4">
-            <input 
-                type="text" 
-                placeholder="Search By: Make, Model, or Type" 
-                className="form-control" 
-                onChange={(event) =>{setSearchTerm(event.target.value)}}
-            />
-                {props.cars.filter(value =>{
-                    if (SearchTerm === ""){
-                        return value
-                    } else if(
-                        value.make.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
-                        || value.model.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
-                        || value.type.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
-                    ){
-                        return value
-                    }
-                }).map((car) =>(
+        <div className="container">
+
+            <Row xs={1} md={3} className="g-4">
+                <input 
+                    type="text" 
+                    placeholder="Search By: Make, Model, or Type" 
+                    className="form-control input-sm" 
+                    onChange={(event) =>{setSearchTerm(event.target.value)}}
+                />
+                    {props.cars.filter(value =>{
+                        if (SearchTerm === ""){
+                            return value
+                        } else if(
+                            value.make.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
+                            || value.model.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
+                            || value.type.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
+                        ){
+                            return value
+                        }
+                    }).map((car) =>(
                         <Col>
                             <Card>
-                                <Card.Img variant="top" src=""/>
+                                <Card.Img variant="top" src="staticImages\Ford_Shelby.jpg"/>
                                 <Card.Body>
-                                    <Card.Title>{car.make, car.model}</Card.Title>
+                                    <Card.Title>{car.make} {car.model}</Card.Title>
                                     <Card.Text>
-                                        {car.price}
+                                        $ {car.price}
+                                    </Card.Text>
+                                    <hr />
+                                    <h6 className="font-weight-bold"> Description </h6>
+                                    <Card.Text>
                                         {car.description}
                                     </Card.Text>
                                 </Card.Body>
+                                <Button varian="success" size="lg">
+                                    <span class="material-icons">add_shopping_cart</span>
+                                    Add to Cart
+                                </Button>
                             </Card>
                         </Col>
-                ))}
-        </Row>
+                    ))}
+            </Row>
+        </div>
     );
 }
 
