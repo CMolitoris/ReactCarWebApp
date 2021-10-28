09bf2092251f4f4b96961baf643d8bbee571a71d
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-28 11:22:36 -0400

    removed unnecessary import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Redirect, Switch, Route } from 'react-router';
 import './App.css';
-import { useLocation } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 import NavBar from './components/Navbar/Navbar';
 import axios from 'axios';
