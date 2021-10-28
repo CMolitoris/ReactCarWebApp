b2dfb70128fb7c008beef70a0f3ca51450b9f2dd
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-28 10:04:53 -0500

    Nav Bar Style | Bootstrap Icons implemented
    Nav Bar Style | Bootstrap Icons implemented
    
    -Do NPM install

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,37 +5,37 @@ import { Link } from "react-router-dom";
 const NavBar = (props) => {
     return ( 
         <nav>
-            <ul className='nav justify-content-end'>
-                <li className='nav-item'><Link to  = '/' className='nav-link'>Home</Link></li>
+            <ul className='nav justify-content-center offset-lg-4'>
+                <li className='nav-item'><Link to  = '/' className='nav-link'><i class="bi bi-house"></i> | Home</Link></li>
                 {!props.user && 
                     <React.Fragment>
                         <li >
-                            <a className='nav-link' onClick = {props.toggleLogModal}>Log In</a>
+                            <a className='nav-link' onClick = {props.toggleLogModal}><i class="bi bi-door-closed"></i> | Log In</a>
                         </li>
                         <li className='nav-item'>
-                            <a className='nav-link' onClick = {props.toggleRegModal}>Register</a>
+                            <a className='nav-link' onClick = {props.toggleRegModal}><i class="bi bi-person-check"></i> | Register</a>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/products' className='nav-link'> Products </Link>
+                            <Link to='/products' className='nav-link'><i class="bi bi-truck"></i> | Products </Link>
                         </li>
                     </React.Fragment>
                 }
                 {props.user && 
                     <React.Fragment>
                         <li className='nav-item'>
-                            <Link to='/account' className='nav-link'>Account</Link>
+                            <Link to='/account' className='nav-link'><i class="bi bi-person"></i> | Account</Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/cart' className='nav-link'>Cart</Link>
+                            <Link to='/cart' className='nav-link'><i class="bi bi-cart4"></i> | Cart</Link>
                         </li>
                         <li className='nav-item'>
-                            <a onClick = {props.logoutUser} className='nav-link'>Log Out</a>
+                            <a onClick = {props.logoutUser} className='nav-link'><i class="bi bi-door-open"></i> | Log Out</a>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/seller' className="nav-link">Sellers</Link>
+                            <Link to='/seller' className="nav-link"><i class="bi bi-currency-dollar"></i> | Sellers</Link>
                         </li>   
                         <li className='nav-item'>
-                            <Link to='/products' className='nav-link'> Products </Link>
+                            <Link to='/products' className='nav-link'><i class="bi bi-truck"></i> | Products </Link>
                         </li>
                     </React.Fragment>
                 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 ReactDOM.render(
   <Router>
