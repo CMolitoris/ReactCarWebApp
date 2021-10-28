4cb722e7243d28c31bccb5f2a9e16492894f82fb
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-28 01:03:46 -0400

    Updated CarDetails component with data.
    - Removed the need for the Car state.
    - Added filter method to grab the selected car from the props state.
    - Created related cars filter side panel (Filtered by Type).
    - Added main car details card with data required from User Stories.
    - TODO:  Add Accordion for Review Section

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -1,47 +1,80 @@
 import React from 'react';
-import { Card, Button, Image} from 'react-bootstrap';
+import { Card, Button, Accordion } from 'react-bootstrap';
 
 
 function CarDetails(props) {
-    console.log(props.car)
-    console.log("test")
+
+    const car = props.cars.filter(car => car.id === props.location.state.carID)
+    
     return ( 
         <div className="row">
             <div className="col-sm-3 bg-danger">
-                TODO: Side Panel for related cars
-                {/* {props.cars.filter(value => value.type === props.car[0].type).map((car)=>(
-                     <Card>
-                     <Card.Img variant="top" src="staticImages\Ford_Shelby.jpg"/>
-                     <Card.Body>
-                         <Card.Title>{car.make} {car.model}</Card.Title>
-                         <Card.Text>
-                             $ {car.price}
-                         </Card.Text>
-                         <hr />
-                         <h6 className="font-weight-bold"> Description </h6>
-                         <Card.Text>
-                             {car.description}
-                         </Card.Text>
-                     </Card.Body>
-                     <Button onClick={() => props.addToCart({
-                             UserId: props.user.id,
-                             CarId: car.id,
-                             Quantity: 1
-                         })} variant="success" size="lg">
-                         <span class="material-icons">add_shopping_cart</span>
-                         Add to Cart
-                     </Button>
-                 </Card>
-                ))} */}
+                <div className="container">
+                    <h4>Related Cars</h4>
+                    {/* Related Cars - Filters cars by Type and excludes the current car from the list */}
+                    {props.cars.filter(
+                        value => value.type.toLowerCase() === car[0].type.toLowerCase() 
+                        && value.id != car[0].id
+                    )
+                    .map((car)=>(
+                        <Card className="mb-4">
+                            <Card.Img variant="top" src="staticImages\Ford_Shelby.jpg"/>
+                            <Card.Body>
+                                <Card.Title>{car.make} {car.model}</Card.Title>
+                                <Card.Text>
+                                    $ {car.price}
+                                </Card.Text>
+                                <hr />
+                                <Card.Text>
+                                    {car.description}
+                                </Card.Text>
+                            </Card.Body>
+                            <Button onClick={() => props.addToCart({
+                                    UserId: props.user.id,
+                                    CarId: car.id,
+                                    Quantity: 1
+                                })} variant="success btn-sm">
+                                <span class="material-icons">add_shopping_cart</span>
+                                Add to Cart
+                            </Button>
+                        </Card>
+                    ))}
+                </div>
             </div>
-            <div className="col-sm-4">
-                <Image src="staticImages\Ford_Shelby.jpg" fluid />
-            </div>
-            <div className="col-sm-4">
+            <div className="col-md-8 card">
                 <Card>
+                    <div className="col-md-6 container mt-3">
+                        <Card.Img src="staticImages\Ford_Shelby.jpg" fluid />
+                    </div>
                     <Card.Body>
-                        <Card.Title>{props.car.make}</Card.Title>
-                        <Card.Text>{props.car[0].make}</Card.Text>
+                        <Card.Title>{car[0].make} {car[0].model}</Card.Title>
+                        <Card.Text>
+                            <h6>${car[0].price}</h6>
+                        </Card.Text>
+                        <hr />
+                        <Card.Text>
+                            <h6>Average Rating: {car[0].averageRating}/5</h6>
+                        </Card.Text>
+                        <hr />
+                        <Card.Text>
+                            {car[0].description}
+                        </Card.Text>
+                        <Card.Text>
+                            Type: {car[0].type} | Make: {car[0].make} | Model: {car[0].model}
+                        </Card.Text>
+                        <Card.Text>
+                            Mileage: {car[0].mileage}
+                        </Card.Text>
+                        <hr />
+                        <Button onClick={() => props.addToCart({
+                                        UserId: props.user.id,
+                                        CarId: car.id,
+                                        Quantity: 1
+                                    })} variant="success">
+                                    <span class="material-icons">add_shopping_cart</span>
+                                    Add to Cart
+                                </Button> 
+                     
                     </Card.Body>
                 </Card>
             </div>
