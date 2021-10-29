199a8089b52af40201ec362c677ff23021a7c2c4
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-29 17:53:45 -0400

    Added form validation to rating form.

--- src/components/RatingForm/RatingForm.jsx
+++ src/components/RatingForm/RatingForm.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { FormControl, Form, Button, FloatingLabel, Row, Col } from 'react-bootstrap';
-
+import {FormControl, Button, FloatingLabel, Row, Col, InputGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
 
 const RatingForm = (props) => {
 
@@ -10,6 +10,8 @@ const RatingForm = (props) => {
         CarId: "", 
     })
 
+    const [validated, setValidated] = useState(false)
+
     const handleChange = (event) => {
         setRatingValues(prevstate => ({
             ...prevstate,
@@ -18,35 +20,48 @@ const RatingForm = (props) => {
     }
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        props.postRating({
-            RatingScore: parseFloat(ratingValues.RatingScore),
-            CarId: props.carID,
-            Message: ratingValues.Message,
-            UserName: props.username
-        })
-        setRatingValues(() => ({
-            RatingScore: "", 
-            Message: "", 
-            CarId: "", 
-        }))
+        const form = event.currentTarget
+        if (form.checkValidity() === false) {
+            event.stopPropagation();
+        } else {
+            props.postRating({
+                RatingScore: parseFloat(ratingValues.RatingScore),
+                CarId: props.carID,
+                Message: ratingValues.Message,
+                UserName: props.username
+            })
+            setRatingValues(() => ({
+                RatingScore: "", 
+                Message: "", 
+                CarId: "", 
+            }))
+        }
+        event.preventDefault();
+        setValidated(true)
     }
 
 
     return ( 
         <Row className="g-2 mb-5">
             <h5 className="text-start p-2">Leave a review!</h5>
-            <Form className="d-flex" onSubmit={handleSubmit}>
+            <Form 
+                className="d-flex" 
+                noValidate
+                validated={validated}
+                onSubmit={handleSubmit}
+            >
                 <Col md="auto">
-                    <Form.Group>
-                        <FloatingLabel controlId="floatingSelectGrid" label="Rating">
+                    <Form.Group controlId="ratingValidation">
+                        <FloatingLabel label="Rating">
                             <Form.Select
+                                type="select"
                                 aria-label='Select rating'
                                 name='RatingScore'
                                 onChange={handleChange}
                                 value={ratingValues.RatingScore}
+                                required
                             >
-                                <option>Select a rating</option>
+                                <option selected disabled value="">Select a rating</option>
                                 <option value="1">1/5 - Poor</option>
                                 <option value="2">2/5 - Below Average</option>
                                 <option value="3">3/5 - Average</option>
@@ -56,9 +71,12 @@ const RatingForm = (props) => {
                         </FloatingLabel>
                     </Form.Group>
                 </Col>
+
+
                 <Col>
-                    <Form.Group>
+                    <Form.Group controlId="messageValidation">
                         <FloatingLabel controlId="floatingInputGrid" label="Review">
+                            <InputGroup hasValidation />
                             <FormControl 
                                 type="text"
                                 placeholder="What's your thoughts..."
@@ -66,11 +84,13 @@ const RatingForm = (props) => {
                                 name='Message'
                                 onChange={handleChange}
                                 value={ratingValues.Message}
+                                required
                             />
+                            <FormControl.Feedback>Looks Good!</FormControl.Feedback>
                         </FloatingLabel>
                     </Form.Group>
                 </Col>
-                <Button variant="primary" type='submit' size = "lg">
+                <Button variant="primary" type='submit' size ="lg">
                     Submit
                 </Button>
             </Form>
