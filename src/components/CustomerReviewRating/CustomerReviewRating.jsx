5058000c89f1e3ac87856465bbc2ef83eba5f06e
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-29 01:40:08 -0400

    Created component to display customer's star rating by review.

---
+++ src/components/CustomerReviewRating/CustomerReviewRating.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function CustomerReviewRating(props) {
+
+    //? Returns an array of strings which will be used to passed into the icon.
+    // ? example: <i className={string} /> / <i className="bi bi-star-fill text-dark" />
+    const customerRating = function() {
+        let stars = []
+        for (let i = 0; i < 5; i++) {
+            if (i < props.ratingScore){
+                stars.push("bi bi-star-fill text-warning")
+            }
+            else {
+                stars.push("bi bi-star-fill text-dark")
+            }
+        }
+        return stars
+    }
+
+
+    return ( 
+        <>
+            {   
+                //? Returns stars array and maps string to className for icon.
+                customerRating(props.ratings).map((star) =>{
+                    return <i className={star}></i>
+                })
+            }
+        </>
+     );
+}
+export default CustomerReviewRating;
