058d36a1c03dc0c48c24021a4d1b96f4f5da9fc3
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-29 20:04:44 -0400

    Moved Related Cars side panel into separate component.

--- src/components/CarDetails/CarDetails.jsx
+++ src/components/CarDetails/CarDetails.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import AddToCartButton from '../AddToCartButton/AddToCartButton';
 import CarAverageRating from '../CarAverageRating/CarAverageRating';
 import RatingSection from '../RatingSection/RatingSection';
+import RelatedCars from '../RelatedCars/RelatedCars';
 import './CarDetails.css'
 
 
@@ -25,35 +26,13 @@ function CarDetails(props) {
                     <div className="col-10 mx-auto">
                         <h4>Related Cars</h4>
                         {/*//? Filters cars by Type and excludes the current car from the list */}
-                        {props.cars.filter(
-                            value => value.type === car.type 
-                            && value.id !== car.id
-                        )
-                        .map((related)=>(
-                            <Card className="mb-4 p-2 shadow" id='card'>
-                                {/*//? Related Cars - Link to car-details */}
-                                <div className=" mt-4">
-                                    <Link to="/car-details" onClick={() => props.getSingleCar(related)}>
-                                        <Card.Img variant="top" src="staticImages\Ford_Shelby.jpg" className='card-image shadow'/>
-                                    </Link>
-                                </div>
-                                <Card.Body>
-                                    <Card.Title>{related.make} {related.model}</Card.Title>
-                                    <Card.Text>
-                                        ${related.price} | {related.type}
-                                    </Card.Text>
-                                    <hr />
-                                </Card.Body>
-                                {/*//? Related Cars - Add to cart btn */}
-                                <div className="mx-auto col-sm-10">
-                                    <AddToCartButton 
-                                        addToCart={props.addToCart} 
-                                        userID={props.user ? props.user.id : null} 
-                                        carID={related.id} 
-                                    /> 
-                                </div>
-                            </Card>
-                        ))}
+                        <RelatedCars 
+                            cars={props.cars} 
+                            car={car} 
+                            user={props.user} 
+                            addToCart={props.addToCart}
+                            getSingleCar={props.getSingleCar} 
+                        />
                     </div>
                 </div>
                 {/*//? CAR DETAILS CARD */}
---
+++ src/components/RelatedCars/RelatedCars.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import AddToCartButton from '../AddToCartButton/AddToCartButton';
+
+
+//? Filters cars by Type and excludes the current car from the list
+function RelatedCars(props) {
+
+    return (
+        <> 
+            {props.cars.filter(
+                value => value.type === props.car.type 
+                && value.id !== props.car.id
+            )
+            .map((related)=>(
+                <Card className="mb-4 p-2 shadow" id='card'>
+                    {/*//? Related Cars - Link to car-details */}
+                    <div className=" mt-4">
+                        <Link to="/car-details" onClick={() => props.getSingleCar(related)}>
+                            <Card.Img variant="top" src="staticImages\Ford_Shelby.jpg" className='card-image shadow'/>
+                        </Link>
+                    </div>
+                    <Card.Body>
+                        <Card.Title>{related.make} {related.model}</Card.Title>
+                        <Card.Text>
+                            ${related.price} | {related.type}
+                        </Card.Text>
+                        <hr />
+                    </Card.Body>
+                    {/*//? Related Cars - Add to cart btn */}
+                    <div className="mx-auto col-sm-10">
+                        <AddToCartButton 
+                            addToCart={props.addToCart} 
+                            userID={props.user ? props.user.id : null} 
+                            carID={related.id} 
+                        /> 
+                    </div>
+                </Card>
+            ))}
+        </>
+     );
+}
+
+export default RelatedCars;
